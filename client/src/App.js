24550738ecf9f114319a490b3ffe643e2a4012dc
sofiapaganin <paganin@berkeley.edu>
2020-11-20 21:46:17 -0800

    physicians and appointments load, cannot parse object due to cors errors

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { physicians: [] };
+    this.state = { physicians: [], appointments: [] };
   }
 
   getPhysicians() {
@@ -15,16 +15,16 @@ class App extends Component {
         .catch(err => err);
   };
 
-
   getAppointments(physicianId) {
     fetch(`http://localhost:9000/appointments/${physicianId}`)
     .then(res => res.text())
-    .then(res => this.setState({ physicians: res }))
+    .then(res => this.setState({ appointments: res }))
     .catch(err => err);
   }
 
   componentDidMount() {
     this.getPhysicians()
+    this.getAppointments('b2');
   }
   render() {
     return (
@@ -33,7 +33,7 @@ class App extends Component {
           Notable App
         </header>
         <p className="App-intro">{this.state.physicians}</p>
-        <div>
+        <div className="sections">
           {/* {
             JSON.parse(`{"data": ${this.state.physicians}}`).data.forEach( physician => {
               return (
@@ -41,6 +41,24 @@ class App extends Component {
               )
             })
           } */}
+          <div>
+            <h2>Physicians</h2>
+            <p
+              // onClick={this.getAppointments('b2')}
+            >Krieger, Algernop</p>
+          </div>
+          <div>
+            <h2>Appointments</h2>
+            <p>{this.state.appointments}</p>
+            <table>
+              <tr>
+                1
+                <td>Sterling Archer</td>
+                <td>8:00AM</td>
+                <td>New Patient</td>
+              </tr>
+            </table>
+          </div>
 
         </div>
       </div>
